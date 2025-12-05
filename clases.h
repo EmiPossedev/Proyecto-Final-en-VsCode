e58015e2eb13d3fba17f69ac25e04cf827ec5651
EmiPosse <posseemiliano2@gmail.com>
2025-12-05 14:04:59 -0300

    Agregué archivos iniciales del proyecto

---
+++ clases.h
@@ -0,0 +1,14 @@
+class Persona{
+private:
+
+public:
+
+
+};
+
+class Kinesiologo : public Persona{
+private:
+
+public:
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "clases.h"
+
+int main(){
+
+
+
+    return 0;
+}
