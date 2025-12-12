5e703daeb653664e29be972cae9074227f1bc675
EmiPosse <posseemiliano2@gmail.com>
2025-12-11 21:49:24 -0300

    Agregué métodos

--- Persona.cpp
+++ Persona.cpp
@@ -1,6 +1,7 @@
 #include "Persona.h"
 /// Implementación de los métodos de la clase Persona
 
+// Métodos para el nombre
 void Persona::setNombre(string nuevoNombre){
     nombre = nuevoNombre;
 }
@@ -8,38 +9,42 @@ string Persona::getNombre(){
     return nombre;
 }
 
+// Métodos para el apellido
 void Persona::setApellido(string nuevoApellido){
     apellido = nuevoApellido;
 }
-
 string Persona::getApellido(){
     return apellido;
 }
 
+// Métodos para el DNI
 void Persona::setDni(int nuevoDni){
     dni = nuevoDni;
 }
-
-int getDni(){
+int Persona::getDni(){
     return dni;
 }
 
-int Persona::getDni(){
-    return 
-}
-
+// Métodos para el mail
 void Persona::setMail(string nuevoMail){
     mail = nuevoMail;
 }
-
 string Persona::getMail(){
     return mail;
 }
 
+// Métodos para el teléfono
 void Persona::setTelefono(int nuevoTelefono){
     telefono = nuevoTelefono;
 }
-
 int Persona::getTelefono(){
     return telefono;
+}
+
+// Métodos para la fecha de nacimiento
+void setFechaDeNacimiento(int nuevaFecha){
+    fechaDeNacimiento = nuevaFecha;
+}
+int getFechaDeNacimiento(){
+    return fechaDeNacimiento;
 }
