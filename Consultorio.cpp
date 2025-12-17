5b0b6c2cfd2410b46d5cf04b82cee25cbbf32044
tomasbourquin <bourquintomas@gmail.com>
2025-12-17 19:40:06 -0300

    metodo importante remprogramarTurno y sus derivados

--- Consultorio.cpp
+++ Consultorio.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <fstream>
 #include <cstring>
+#include <iostream> 
 
 // Método Destructor
 Consultorio::~Consultorio()
@@ -61,8 +62,8 @@ void Consultorio::cancelarTurno(const string &nombrePacienteBuscado, const strin
 {
     for (auto it = turnos.begin(); it != turnos.end(); it++) //tenias auto i, no auto it;
     {
-        if (it->nombrePaciente == nombrePacienteBuscado && 
-            it->fecha == fecha && // En minúsculas
+        if (it->nombrePaciente == nombrePacienteBuscado and 
+            it->fecha == fecha and // En minúsculas
             it->hora == hora)     // En minúsculas
         {
             turnos.erase(it);
@@ -71,3 +72,118 @@ void Consultorio::cancelarTurno(const string &nombrePacienteBuscado, const strin
     }  
 } 
 
+
+// Verificación 1: Disponibilidad del Kinesiólogo 
+bool Consultorio::verificarDisponibilidadKinesiologo(const string &nombreKinesio, const string &fecha, const string &hora)
+{
+    for (const auto &turno : turnos)
+    {
+        // Si el kinesiólogo ya tiene turno ese día a esa hora...
+        if (turno.nombreKinesiologo == nombreKinesio and turno.fecha == fecha and turno.hora == hora)
+        {
+            if (turno.estadoDelTurno != "Cancelado") 
+            {
+                return false; 
+            }
+        }
+    }
+    return true; // Está libre
+}
+
+// Verificación 2: Disponibilidad de Camillas 
+bool Consultorio::verificarDisponibilidadCamilla(const string &fecha, const string &hora)
+{
+    int camillasOcupadas = 0;
+
+    for (const auto &turno : turnos)
+    {
+        // Buscamos turnos a la misma hora que usen camilla
+        if (turno.fecha == fecha and  turno.hora == hora and turno.requiereCamilla and turno.estadoDelTurno != "Cancelado")
+        {
+            camillasOcupadas++;
+        }
+    }
+
+    if (camillasOcupadas < cantCamillas)
+    {
+        return true; // Hay lugar
+    }
+    else
+    {
+        return false; // Todo lleno
+    }
+}
+
+// Verificación 3: Disponibilidad del Gimnasio 
+bool Consultorio::verificarDisponibilidadGimnasio(const string &fecha, const string &hora)
+{
+    int gimnasioOcupado = 0;
+
+    for (const auto &turno : turnos)
+    {
+        // Buscamos turnos a la misma hora que usen el gimnasio
+        if (turno.fecha == fecha and turno.hora == hora and turno.requiereGimnasio and turno.estadoDelTurno != "Cancelado")
+        {
+            gimnasioOcupado++;
+        }
+    }
+
+    // Verificamos límite (capacidadGimnasio está definida en el .h como 5)
+    if (gimnasioOcupado < capacidadGimnasio)
+    {
+        return true; // Hay lugar
+    }
+    else
+    {
+        return false; // Todo lleno
+    }
+}
+
+void Consultorio::reprogramarTurno(const string &nombrePaciente, const string &fechaVieja, const string &horaVieja, const string &fechaNueva, const string &horaNueva)
+{
+    for (auto &turno : turnos)
+    {
+        // Buscar el turno que coincida con paciente, fecha vieja y hora vieja
+        if (turno.nombrePaciente == nombrePaciente and
+            turno.fecha == fechaVieja and
+            turno.hora == horaVieja)
+        {
+           
+            // Verificar si el MISMO kinesiólogo está libre en el nuevo horario
+            if (!verificarDisponibilidadKinesiologo(turno.nombreKinesiologo, fechaNueva, horaNueva))
+            {
+                cout << "Error: El kinesiologo " << turno.nombreKinesiologo << " ya tiene un turno a esa hora nueva." << endl;
+                return; 
+            }
+
+            if (turno.requiereCamilla)
+            {
+                if (!verificarDisponibilidadCamilla(fechaNueva, horaNueva))
+                {
+                    cout << "Error: No hay camillas disponibles en el nuevo horario." << endl;
+                    return;
+                }
+            }
+
+            // Verificar recursos (Gimnasio) SOLO si el turno original usaba gimnasio
+            if (turno.requiereGimnasio)
+            {
+                if (!verificarDisponibilidadGimnasio(fechaNueva, horaNueva))
+                {
+                    cout << "Error: El gimnasio esta lleno en el nuevo horario." << endl;
+                    return;
+                }
+            }
+            // Si todas las verificaciones pasan, de 10
+            turno.fecha = fechaNueva;
+            turno.hora = horaNueva;
+            turno.estadoDelTurno = "Reprogramado"; 
+            
+            cout << "El turno ha sido reprogramado correctamente." << endl;
+            return; 
+        }
+    }
+
+    // Si termina el for y no retornó, es que no encontró el turno viejo
+    cout << "Error: No se encontro el turno original para reprogramar." << endl;
+}
