7a6e34b8c5cf7b041440a5ae2a5cd4fbe5f97815
tomasbourquin <bourquintomas@gmail.com>
2025-12-20 09:26:30 -0300

    comenzando el main (la lluvia me impide seguir, en un rato sigo)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,23 @@
 
 int main(){
 
-    
+    Consultorio sistema;
+    sistema.cargarTodosDatos();
 
+    int opcion;
+    do {
+        cout << " SISTEMA DE GESTION DE CONSULTORIO " << endl;
+        cout << "1. Gestionar Pacientes" << endl;
+        cout << "2. Gestionar Kinesiologos" << endl;
+        cout << "3. Gestionar Turnos" << endl;
+        cout << "0. Guardar y Salir" << endl;
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
+
+        switch(){
+            
+        }
     return 0;
+
+
 }
